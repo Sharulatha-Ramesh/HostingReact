9b4a5302af1d3ec01a9e4c6609c3932ad2ef8387
Sharulatha R <sharulatha2205@gmail.com>
2024-05-03 09:42:40 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,6 +13,7 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
@@ -94,7 +95,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://www.linkedin.com/in/sharulatha22/'
             >
               Linkedin <FaLinkedin size={30} />
             </a>
@@ -102,7 +103,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://github.com/Sharulatha-Ramesh'
             >
               Github <FaGithub size={30} />
             </a>
@@ -110,19 +111,20 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href={`mailto:sharulatha2205@gmail.com`}
             >
               Email <HiOutlineMail size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
-            >
-              Resume <BsFillPersonLinesFill size={30} />
-            </a>
-          </li>
+          <a
+            className='flex justify-between items-center w-full text-gray-300'
+            href='https://drive.google.com/file/d/12aG5orS48TkcVUWGQ3FyehOaFrNGecEK/view?usp=sharing'
+            download='resume.pdf'
+          >
+            Resume <BsFillPersonLinesFill size={30} />
+          </a>
+        </li>
         </ul>
       </div>
     </div>
